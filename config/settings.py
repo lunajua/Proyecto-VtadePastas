e6508dde6efdb8488c0ad8fc53e50498f7cf5b69
Lucas <lucasbecca3@hotmail.com>
2024-03-20 21:03:31 -0300

    all

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "pastas",
     'crispy_forms',
     'crispy_bootstrap5',
+    'usuarios'
     
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('urls.pastas')),
+    path("", include('pastas.urls')),
+    path("usuarios/", include('usuarios.urls')),
     
 ]
