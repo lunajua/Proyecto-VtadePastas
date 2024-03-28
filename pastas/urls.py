c7f330fd6e64846657d799250a69dcd878ac36be
Juan Pablo Luna <jp_luna@hotmail.com>
2024-03-28 16:30:16 -0300

    Redirecciones y Herencia

--- pastas/urls.py
+++ pastas/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path 
 from pastas import views
 
+app_name = 'pastas'  # Nombre de la aplicacion para poder usarla en el template
+
 urlpatterns = [
     
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contacto, name="contacto"),
+    path('localizar/', views.localiza, name="localizar"),
+    path('menu/', views.menu, name="menu"),
 
 ]
--- pastas/views.py
+++ pastas/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pastas/index.html')
+
+def about(request):
+    return render(request, 'pastas/about.html')
+
+def localiza(request):
+    return render(request, 'pastas/localizar.html')
+
+def menu(request):
+    return render(request, 'pastas/menu.html')
+
+def contacto(request):
+    return render(request, 'pastas/contacto.html')
+
